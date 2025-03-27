6d4c338f98b1e1d8faed39a7328d5415216f3af1
Galaxkkkk <galaxy.o.peixoto@gmail.com>
2025-03-27 16:35:13 -0300

    Build frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,7 @@
-export default {
+import { defineConfig } from 'vite'
+export default defineConfig({
   build: {
-    outDir: '../backend/public'  // Ou '/app/public' para o build no container
+    outDir: '../backend/public',
+    emptyOutDir: true
   }
-}
+})
