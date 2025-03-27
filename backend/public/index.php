bb9e3abe76d93c88f8db47f0008e704e9d99d2dd
Galaxkkkk <galaxy.o.peixoto@gmail.com>
2025-03-27 16:16:49 -0300

    a

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,2 +1,10 @@
 <?php
-require __DIR__.'/../api/patients/read.php';
+  // Roteamento básico
+  $request = $_SERVER['REQUEST_URI'];
+  if (str_starts_with($request, '/api')) {
+      require __DIR__.'/../'.ltrim($request, '/');
+  } else {
+      header('Content-Type: text/html');
+      readfile(__DIR__.'/index.html');
+  }
+?>
